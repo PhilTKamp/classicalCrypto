8e9dfd305611dfaebe0489b9abaa758e21323213
Telkamp, Philip <philip.telkamp@mines.sdsmt.edu>
2019-01-30 12:05:54 -0700

    Added initial script to open a file and parse text, functions written to final all occurences of a substring, functions remai untested

---
+++ genHist.py
@@ -0,0 +1,40 @@
+import pandas
+import matplotlib
+from collections import Counter
+
+def getDistances( text, displacement ):
+	distances = [];
+
+
+# Accepts the substring, the overall text to find
+# Will return a list of all indexes where it found the string, including the first occurence 
+def findAll( substr, text ):
+	indicies = []
+
+	# If the substr didn't exist, kick out
+	if(text.find(substr) == -1):
+		return;
+
+	# Finds the starting point for our text
+	indicies.append(text.find(substr));
+
+	# The point where we start our search, first index + the length of the substring to avoid overlap
+	startSearch = indicies[0] + len(substr);
+
+	# While we keep finding matches, keep searching and moving th starting point forward
+	while(text.find(substr, startSearch) != -1):
+
+		# Adds the next found index to our list and gets a new point to start the search from
+		indicies.append(text.find(substr, start));
+		startSearch = indicies[len(indicies) - 1] + len(substr);
+
+	return indicies;
+
+
+print("Enter the file to generate a histogram for: ");
+file = input();
+
+fileObj = open(file, 'r');
+text = fileObj.read();
+
+print(text);
