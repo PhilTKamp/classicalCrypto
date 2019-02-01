ee426e745eb5de9934c4f120185c60177f66e463
Telkamp, Philip <philip.telkamp@mines.sdsmt.edu>
2019-02-01 12:59:07 -0700

    Added function to get the X2 of the stats of a char set and return the difference to the English Language X2

--- getKeyword.py
+++ getKeyword.py
@@ -1,3 +1,6 @@
+#alphabet = [ 0.0815, 0.0144, 0.0276, 0.0379, 0.1311, 0.0292, 0.0199, 0.0526, 0.0635, 0.0013, 0.0042, 0.0339, 0.254, 0.071, 0.08, 0.0198, 0.0012, 0.0683, 0.061, 0.1047, 0.0246, 0.0092, 0.0154, 0.0017, 0.0198, 0.0008 ]
+
+
 # Accepts a string of text and the keyLength to divide that string upon
 # Returns a 2D list where each 1D list is the coset of the original text
 def createCoSets(text, keyLength):
@@ -18,6 +21,19 @@ def shiftText( text, shift ):
 
 	for character in text:
 		shiftedText.append()
+
+def getXSquaredDiff( charStatistics ):
+	num = 0.0;
+	den = 0.0;
+	IC = 0.0667;
+	for stat in charStatistics:
+		i = stat
+		num += stat*(stat-1);
+		den += stat;
+	if ( den == 0.0):
+		return 0.0;
+	else:
+		return IC - num / ( den * ( den - 1 ));
 
 
 print("Enter the file of ciphertext to generate the cosets and keyword for: ");
